32b7556ec2eb42efe4ec8acbc8c71a1c5f3263d7
Dennis Witt <witt.dennis@outlook.com>
2023-02-22 20:59:50 +0100

    ory working turbo monorepo template

--- tools/setup-ory-kratos.sh
+++ tools/setup-ory-kratos.sh
@@ -6,4 +6,4 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 helm repo add ory https://k8s.ory.sh/helm/charts
 helm repo update
 
-helm upgrade --install -f ${SCRIPT_PATH}/../manifests/kratos/values.yaml ory/kratos
+helm upgrade --install --namespace ory-playground -f ${SCRIPT_PATH}/../manifests/kratos/values.yaml ory-kratos ory/kratos --debug
